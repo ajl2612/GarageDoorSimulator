013e719d675bd4e92e3115d7cd1591d171ef3739
Andrew Lyne <andylyne2612@gmail.com>
2014-03-20 02:27:27 -0400

    Initial Commit

---
+++ GarageDoorSimulator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GarageDoorSimulator
+//
+//  Created by Andrew III Lyne on 3/20/14.
+//  Copyright (c) 2014 Andrew III Lyne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
